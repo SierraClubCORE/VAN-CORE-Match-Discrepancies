7ac689c7d20b6012ccdf1b4309fa2474c9629f7f
oliverwhit <66801422+oliverwhit@users.noreply.github.com>
2021-06-29 16:46:09 -0400

    Update 1a.sql

--- 1a.sql
+++ 1a.sql
@@ -1,6 +1,5 @@
 -- VAN records that have an SFID listed. This SFID corresponds to a different VANID and VAN state in Salesforce
 
-
 -- 'Sierra Club Foundation (c3)' Committee
 CREATE TEMPORARY TABLE step1 AS
 SELECT a.statecode, a.vanid, b.salesforceid, a.firstname||' '||a.lastname AS van_fullname, a.committeeid, a.createdby
@@ -18,7 +17,7 @@ WHERE a.vanid <> b.van_id_foundation__c
     AND a.createdby = 1498043
  	AND a.committeeid = 57277;
 	
-	
+
 -- 'Sierra Club (c4)' Committee
 CREATE TEMPORARY TABLE step1 AS
 SELECT a.statecode, a.vanid, b.salesforceid, a.firstname||' '||a.lastname AS van_fullname, a.committeeid, a.createdby
