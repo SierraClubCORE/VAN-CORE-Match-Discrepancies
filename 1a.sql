817edd964d778afb989e704a6f3fc13fe2c05bee
oliverwhit <66801422+oliverwhit@users.noreply.github.com>
2021-06-29 16:45:27 -0400

    Update and rename 1a-c4-committee.sql to 1a.sql

--- 1a.sql
+++ 1a.sql
@@ -1,5 +1,25 @@
 -- VAN records that have an SFID listed. This SFID corresponds to a different VANID and VAN state in Salesforce
 
+
+-- 'Sierra Club Foundation (c3)' Committee
+CREATE TEMPORARY TABLE step1 AS
+SELECT a.statecode, a.vanid, b.salesforceid, a.firstname||' '||a.lastname AS van_fullname, a.committeeid, a.createdby
+FROM sc_van_staging.tsm_sc_contacts_myc a
+LEFT JOIN sc_van_staging.tsm_sc_contactsexternalids b ON
+	a.statecode = b.statecode
+	AND a.vanid = b.vanid
+	AND a.committeeid = b.committeeid;
+
+SELECT a.statecode, a.vanid, a.van_fullname, b.id AS sf_id, b.van_state__c AS sf_van_state__c, b.van_id_foundation__c as sf_van_id_foundation__c, b.mailingstate as sf_mailingstate
+FROM step1 a
+JOIN sc_analytics.salescloud_contact_w_source b
+    ON a.salesforceid = b.id
+WHERE a.vanid <> b.van_id_foundation__c
+    AND a.createdby = 1498043
+ 	AND a.committeeid = 57277;
+	
+	
+-- 'Sierra Club (c4)' Committee
 CREATE TEMPORARY TABLE step1 AS
 SELECT a.statecode, a.vanid, b.salesforceid, a.firstname||' '||a.lastname AS van_fullname, a.committeeid, a.createdby
 FROM sc_van_staging.tsm_sc_contacts_myc a
